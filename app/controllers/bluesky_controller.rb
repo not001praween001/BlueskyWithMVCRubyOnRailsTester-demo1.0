95e61c4c466c7dbcfa65099781b63a6e103f572a
Praween AMONTAMAVUT <not001praween001@gmail.com>
2015-03-02 06:35:03 +0900

    Fix identify EDIP

--- app/controllers/bluesky_controller.rb
+++ app/controllers/bluesky_controller.rb
@@ -11,9 +11,12 @@ class BlueskyController < ApplicationController
     # @Author: Praween AMONTAMAVUT                                              #
     #############################################################################
 
+    # Identify your bluesky domain
     your_bluesky_domain = 'http://localhost:8189'
-    # Blue-sky Login sample
+    # Identify your embedded device IP address
+    your_local_ed_ip = '192.168.1.22'
 
+    # Blue-sky Login sample
     login_uri = URI.parse(your_bluesky_domain + '/doLogin.ins')
 
     num = 0
@@ -26,7 +29,7 @@ class BlueskyController < ApplicationController
     puts "Login result: " + login_result
 
     # Blue-sky set data example
-    url = URI.parse(your_bluesky_domain + '/ETLog?instruction=sensornetwork&opt1=172.16.4.222&opt2=gpio&opt3=set&opt4=22&opt5=0')
+    url = URI.parse(your_bluesky_domain + '/ETLog?instruction=sensornetwork&opt1=' + your_local_ed_ip + '&opt2=gpio&opt3=set&opt4=22&opt5=0')
     req = Net::HTTP::Get.new(url.to_s)
     res = timeout(120) { Net::HTTP.start(url.host, url.port) {|http|
       http.use_ssl = false
@@ -45,6 +48,6 @@ class BlueskyController < ApplicationController
        logout_result = logout_res_hash["ETLog"]["logout"]["result"]
     end while num < 2 && logout_result == "false"
     puts "Logout result: " + logout_result
-    render :text => "We has set the value 0 to gpio pin number 22 of the local embedded device(ED) IP '172.16.4.222' of public domain #{your_bluesky_domain}. The operation result is '#{set_result}'"
+    render :text => "We has set the value 0 to gpio pin number 22 of the local embedded device(ED) IP '#{your_local_ed_ip}' of public domain #{your_bluesky_domain}. The operation result is '#{set_result}'"
   end
 end
